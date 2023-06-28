2ad2c218de71a8f906a2a15fb71553bbbffb7cdc
Mindia Shantadze <129517313+mindiashantadze@users.noreply.github.com>
2023-06-28 18:14:37 +0400

    Initial commit

---
+++ src/test/java/mshantadze/carina/ProxyTest.java
@@ -0,0 +1,38 @@
+package mshantadze.carina;
+
+import com.browserup.bup.BrowserUpProxy;
+import com.browserup.bup.proxy.CaptureType;
+import com.zebrunner.carina.core.AbstractTest;
+import com.zebrunner.carina.proxy.browserup.ProxyPool;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProxyTest extends AbstractTest {
+    private static final Logger LOGGER = LogManager.getLogger(ProxyTest.class);
+
+    @Test
+    public void proxyTest() {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        BrowserUpProxy proxy = ProxyPool.getProxy();
+        proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT,CaptureType.RESPONSE_CONTENT);
+        List<String> requestUrls = proxy.getHar().getLog().getEntries()
+                .stream()
+                .map((entry) -> {
+                    String url = entry.getRequest().getUrl();
+                    LOGGER.info(url);
+                    return url;
+                })
+                .collect(Collectors.toList());
+
+        Assert.assertTrue(
+                requestUrls.stream().anyMatch((url) -> url.contains("https://rest.happierleads.com")),
+                "Request is not sent to https://rest.happierleads.com"
+        );
+    }
+}
